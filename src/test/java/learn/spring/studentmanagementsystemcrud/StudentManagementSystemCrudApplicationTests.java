98454c108ba329dbee3ce72e887fd320c41d8736
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-12-14 15:31:36 +0545

    Project created

---
+++ src/test/java/learn/spring/studentmanagementsystemcrud/StudentManagementSystemCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.spring.studentmanagementsystemcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
