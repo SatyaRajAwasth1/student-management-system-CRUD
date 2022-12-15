396a33d558e5f9809f4df801253fe36844635fc5
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2022-12-15 23:25:47 +0545

    entity srudent and properties added

--- src/main/java/learn/spring/studentmanagementsystemcrud/Entity/Student.java
+++ src/main/java/learn/spring/studentmanagementsystemcrud/Entity/Student.java
@@ -1,2 +1,59 @@
-package learn.spring.studentmanagementsystemcrud.Entity;public class Student {
+package learn.spring.studentmanagementsystemcrud.Entity;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    // Student a Entity having
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name", nullable = false) //for custom name and not null
+    private String fName;
+    @Column(name = "last_name", nullable = false)
+    private String lName;
+    private String email; //will create column name as of the field i.e. email if not assigned
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Student (){
+
+    }
+
+    public Student(String fName, String lName, String email) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+    }
 }
